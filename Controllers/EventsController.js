b52ff0b409250d8ea5f43aba372c5dc7273212a4
Lean Hoyos <hoyos009@gmail.com>
2021-10-15 15:19:18 -0300

    Rutas principales sin validaciones terminadas

--- Controllers/EventsController.js
+++ Controllers/EventsController.js
@@ -1,13 +1,51 @@
 const Event = require('../models/Event');
 
 
-const getEvents = async (req,res) => {
+const addEvents = async function(req, res){
+    try{
+        const event = new Event(req.body);
+        const result = await event.save();
+        res.send(result);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+const getEvents = async function(req, res){
     try {
         const events = await Event.find();
         res.send(events)
-    } catch (err) {
-        console.log(err) 
     } 
-};
+    catch (err) {
+        console.log(err)
+    }
+}
 
-module.exports = { getEvents };
+const getEventDetail = async function(req, res){
+    try {
+        const detail = await Event.findById(req.params);
+        res.send(detail)
+    } 
+    catch (err) {
+        console.log(err)
+    }
+}
+
+const getAssistans = async function(req, res){
+    try{
+        const detail = await Event.findById(req.params);
+        const list = detail.asistentes;
+        res.send(list)
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = {
+    addEvents, 
+    getEvents,
+    getEventDetail,
+    getAssistans
+}
--- Routes/users.js
+++ Routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getUsers, createUser } = require('../Controllers/UsersController');
 const router = express.Router();
 
-router.get('/users', getUsers);
+router.get('/', getUsers);
 router.post('/adduser', createUser);
 
 module.exports = router;
