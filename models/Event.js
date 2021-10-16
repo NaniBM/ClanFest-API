f3441a50a16b9904fbbcb6a3673e91adf3f87409
NaniBM <bazanmurua@gmail.com>
2021-10-16 18:12:29 -0300

    se agrego coordenadas al modelo de events

--- models/Event.js
+++ models/Event.js
@@ -20,11 +20,6 @@ const eventSchema = new Schema({
   direccion: {
     type: String,
     required: true,
-<<<<<<< HEAD
-  },  
-  coordenadas: {
-    type: []
-=======
     validate:{
       validator:(descrip)=>{
         if(descrip.length >= 10){
@@ -35,7 +30,9 @@ const eventSchema = new Schema({
       },
       message: 'Tu descripcion debe ser mayor a 10 caracteres'
     },
->>>>>>> 3c6f6c778a4f58591182b367f337a223db326cf9
+  },
+  coordenadas: {
+    type: []
   },
   precio: {
     type: Number,
