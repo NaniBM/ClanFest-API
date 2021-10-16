a3b2fca5f29788464c453e2c76bd466713109679
NaniBM <bazanmurua@gmail.com>
2021-10-16 02:03:52 -0300

    Access control configurado

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ mongoose.connect(process.env.DB_URI,{
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
