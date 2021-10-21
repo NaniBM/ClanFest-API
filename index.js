da49e8f3ba939a47ca7ed6587ef621229fa8d9e5
NaniBM <bazanmurua@gmail.com>
2021-10-21 14:43:06 -0300

    agregado PATCH a cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
   next();
 });
 
