dc12bcb9067da081ebdbba6a31e4d37c63719a56
Estanip <estani.pettigrew@gmail.com>
2021-10-22 17:39:07 -0300

    agregue endpoint add y get tasks

--- models/User.js
+++ models/User.js
@@ -49,6 +49,9 @@ const userSchema = new Schema({
     type: String,
     default: "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png"
   },
+  tareas: {
+    type: [],
+  },
   fechaDeCreacion: {
     type: Date,
     default: new Date(),
