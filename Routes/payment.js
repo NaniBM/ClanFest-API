7e757281608886f4c84f89464cab385920e0e525
Estanip <estani.pettigrew@gmail.com>
2021-10-26 17:00:45 -0300

    fix git

--- Routes/payment.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {getMercadoPagoLink} = require('../Controllers/paymentController/paymentController');
-
-// crear nuevo link de pago
-router.post('/new', getMercadoPagoLink);
-
-module.exports = router;
