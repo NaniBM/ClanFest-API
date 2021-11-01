1a4d370119921aea50c7d8fd7aa44ad8f403eae1
NaniBM <bazanmurua@gmail.com>
2021-11-01 01:13:28 -0300

    agrego categorias al modelo eventos

--- models/Event.js
+++ models/Event.js
@@ -72,9 +72,6 @@ const eventSchema = new Schema({
     tareasDelUsuario: {
       type:[]
     },
-    categorias: {
-      type:[]
-    },
     statusPago: {
       type: String,
       enum: {
@@ -82,6 +79,9 @@ const eventSchema = new Schema({
       }
     }
   }],
+  categorias: {
+    type:[]
+  },
   imagen: {
     type: String,
     default: "https://images.unsplash.com/photo-1572575156811-2ae050d748d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
