ddc25726ca3951c711db1580ae2aabd267a8602a
Estanip <estani.pettigrew@gmail.com>
2021-11-02 15:32:06 -0300

    trabajando en el endpoint qrpdf

--- Routes/index.js
+++ Routes/index.js
@@ -9,7 +9,6 @@ const emailRoutes = require('./Nodemailer')
 const notLoginRoutes = require('./notLogin');
 const adminRoutes = require('./admin');
 
-
 router.use('/api/users', usersRoutes);
 router.use('/events', eventsRoutes);
 router.use('/api/auth', require('./auth'));
