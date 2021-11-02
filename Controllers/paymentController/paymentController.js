b37c635477503d78269d5f0354beb85d8eb1e50a
Lean Hoyos <hoyos009@gmail.com>
2021-11-02 17:05:53 -0300

    getPayments mejorado

--- Controllers/paymentController/paymentController.js
+++ Controllers/paymentController/paymentController.js
@@ -15,8 +15,11 @@ const getPayments = async (req,res) => {
         const result = await User.find({'eventosaAsistir.statusPago.status': {$in:["Aprobado"]}}).select('eventosaAsistir usuario email').populate('eventosaAsistir.eventId', {
             nombreDelEvento: 1
         }).exec();
-
-        return res.json(result);
+        let filtrado = result.map((u) => {
+            u.eventosaAsistir = u.eventosaAsistir.filter(p => p.statusPago.status === "Aprobado")
+            return u
+        })
+        return res.json(filtrado);
 
     } catch (err) {
         console.error(err)
